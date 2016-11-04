aa1726ffc5ae05e22c631658797d25e18193f595
yuzhiyun <2723364262@qq.com>
2016-11-04 19:22:16 +0800

    servlet判断登录是否成功，只加了一个用户，yuzhiyun,123

---
+++ src/action/UserServlet.java
@@ -0,0 +1,61 @@
+package action;
+
+import sun.rmi.runtime.Log;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by yuzhiyun on 2016-11-04.
+ */
+@WebServlet("*.user")
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.print("访问UserServlet成功" + request.getCharacterEncoding() + " " + response.getCharacterEncoding());
+        System.out.println("访问UserServlet成功" +"username="+request.getParameter("username")+"  password="+request.getParameter("password"));
+
+        String temp = request.getServletPath().substring(1);
+        String methodName = temp.substring(0, temp.length() - 5);
+
+        Method method = null;
+        try {
+            method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, request, response);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 登录
+     * */
+    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        if("yuzhiyun".equals(username) && "123".equals(password)){
+            response.sendRedirect("index.html");
+        }else{
+            response.sendRedirect("error.html");
+        }
+
+        System.out.println("登录用户名"+request.getParameter("username"));
+    }
+
+    public void register(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
